ce400de401f691b68c4a47e44a7d1897cb125977
Jongmin Jack Kim <jmjkim00@icloud.com>
2022-02-08 15:59:14 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(arr, str) {
+    const matchingDriver = arr.filter(driver => driver === str || driver === str.toLowerCase());
+    
+    return matchingDriver;
+}
+
+function fuzzyMatch(arr, str) {
+    const matchingDriver = arr.filter(driver => driver.slice(0, 2) === str || driver.name === str);
+
+    return matchingDriver;
+}
+
+function matchName(arr, str) {
+    const matchingDriver = arr.filter(driver => driver.name === str);
+    
+    return matchingDriver;
+}
